6493f6ce7884be79a1105849303855dcc00dadaa
Schapkun <info@schapkun.com>
2025-07-01 11:39:34 +0200

    Create supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://rybezhoovslkutsugzvv.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5YmV6aG9vdnNsa3V0c3VnenZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMjMwNDgsImV4cCI6MjA2NDc5OTA0OH0.JihNgpfEygljiszxH7wYD1NKW6smmg17rgP1fJcMxBA"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
